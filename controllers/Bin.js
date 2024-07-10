525987d4edfeaffc9f2f5c2925693fbe54b01021
Franstio <franstiodiego01@gmail.com>
2024-07-10 18:09:18 +0700

    ok

--- controllers/Bin.js
+++ controllers/Bin.js
@@ -23,7 +23,7 @@ export const switchLamp = async (id, lampType, isAlive) => {
 export const checkLampRed = async () => {
     
     try {
-        const response = await axios.get(`http://2-PCL.local:5000/getbinData?hostname=${os.hostname()}`, { withCredentials: false,timeout: 500 });
+        const response = await axios.get(`http://${process.env.TIMBANGAN}/getbinData?hostname=${os.hostname()}`, { withCredentials: false,timeout: 500 });
         const bin = response.data.bin;
             
         console.log({ weight :bin.weight,max:bin.max_weight });
@@ -47,7 +47,7 @@ export const checkLampRed = async () => {
 export const checkLampYellow = async () => {
     while (true) {
         try {
-            const response = await axios.get(`http://2-PCL.local:5000/getbinData?hostname=${os.hostname()}`, { withCredentials: false });
+            const response = await axios.get(`http://${process.env.TIMBANGAN}/getbinData?hostname=${os.hostname()}`, { withCredentials: false });
             const bin = response.data;
             console.log({ binFromApi: bin });
             
